1d565865cd74f9b632af2de1125aed6c633f210f
242391 Marta Glinka <242391@edu.p.lodz.pl>
2024-10-31 23:39:10 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
             <div className="text-2xl md:text-3xl font-bold tracking-wide">
                 ControlFood
             </div>
-            <nav className="flex flex-col md:flex-row md:items-center md:space-x-6">
+            <nav className="flex space-x-6">
                 <Link to="/register" className="text-lg md:text-xl font-semibold transition-colors duration-300 hover:text-yellow-400">
                     Register
                 </Link>
